8c6ca2ca3087d83abb6ca8440036d122d93a6c49
Juliette Cordor <juliette@jewelexx.com>
2023-05-24 11:49:52 +1000

    basic returns path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/{path}")]
+async fn hello(path: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(path)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
