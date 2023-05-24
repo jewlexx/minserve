abbe6b03536060f00cdb0aebffe203d3191280d0
Juliette Cordor <juliette@jewelexx.com>
2023-05-24 15:18:18 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use derive_more::{AsRef, Deref, DerefMut, Display, From};
+use std::ops::Deref;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Deref, DerefMut, AsRef)]
+struct StrWrapper(String);
+
+impl StrWrapper {
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+}
 
 #[get("/{path:.**}")]
 async fn hello(path: web::Path<String>) -> impl Responder {
@@ -7,6 +18,9 @@ async fn hello(path: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let int = StrWrapper(String::new());
+    let s = int.to_string();
+
     HttpServer::new(|| App::new().service(hello))
         .bind(("127.0.0.1", 8080))?
         .run()
