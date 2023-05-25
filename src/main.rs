82de1cedd3217a2e0211df3ff29a2aeee46d9ac6
Juliette Cordor <juliette@jewelexx.com>
2023-05-25 10:24:19 +1000

    fixes

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(Files::new("/", "./").index_file("index.html")))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().service(
+            Files::new("/", "./")
+                .index_file("index.html")
+                .prefer_utf8(true)
+                .use_hidden_files()
+                .show_files_listing(),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
